0ef90904f740e4e0e015106f25b01d469064c28a
Felipe Caccianiga Camargo <56848726+felipecaccianiga@users.noreply.github.com>
2021-12-10 00:18:15 -0300

    Update ProjetoFinal.c

--- ProjetoFinal.c
+++ ProjetoFinal.c
@@ -9,24 +9,24 @@ int main(int argc, char* argv[]){
     int i;
     float pi = 0.0;
     float sinal = 1.0;
-    float soma = 0.0;
-    int max = 10^100000; //numero maximo de vezes que os calculos ser„o feitos
+    float soma = 0.0; // aqui ser√£o somados todos os resultados das contas, para depois calcular o valor de pi
+    int max = 10^100000; //numero maximo de vezes que os calculos ser√£o feitos
 
     #pragma omp parallel for num_threads(thread_count) reduction (+:soma) private (sinal)
 
     for (i = 0; i < max; i++) {
-        if(i % 2 == 0){ //se o numero for par o proximo senal È positivo
+        if(i % 2 == 0){ //se o numero for par o proximo sinal √© positivo
             sinal = 1.0;
-        } else {  //se o numero for impar o proximo senal È negativo
+        } else {  //se o numero for impar o proximo sinal √© negativo
             sinal = -1.0;
         }
-        soma += sinal / (2 * i + 1);
-        int my_rank = omp_get_thread_num(); 
-        int thread_count = omp_get_num_threads();
-        printf("Thread %d of %d\n", my_rank, thread_count);
+        soma += sinal / (2 * i + 1); 
+        int my_rank = omp_get_thread_num(); //variavel para mostrar em qual thread estamos
+        int thread_count = omp_get_num_threads(); //variavel para mostrar a contagem total das threads
+        printf("Thread %d of %d\n", my_rank, thread_count); 
     }
 
-    pi = 4 * soma;
+    pi = 4 * soma; 
 
     printf("Valor aproximado de Pi: %f\n", pi);
 
